198a5f9357501c4ebe61a97bd3abe87475f40c4b
Ryan Horrmann <ryanhorrmann123@gmail.com>
2025-05-02 15:42:22 +0200

    Add: Cancellation tokens to repository methods

---
+++ Colabs.ProjectManagement.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using Colabs.ProjectManagement.Application.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Colabs.ProjectManagement.Persistence.Repositories
+{
+    public class BaseRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly ColabsDbContext _dbContext;
+
+        public BaseRepository(ColabsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+           return await _dbContext.Set<T>().FindAsync(id, cancellationToken);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<T>().ToListAsync(cancellationToken);
+        }
+
+        public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            await _dbContext.Set<T>().AddAsync(entity, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            
+        }
+
+        public async Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+        {
+           _dbContext.Entry(entity).State = EntityState.Modified;
+           await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+           var entity = await GetByIdAsync(id, cancellationToken);
+           if (entity != null)
+           {
+               _dbContext.Set<T>().Remove(entity);
+               await _dbContext.SaveChangesAsync(cancellationToken);
+           }
+        }
+        
+    }
+}
---
+++ Colabs.ProjectManagement.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Colabs.ProjectManagement.Application.Contracts.Persistence;
+using Colabs.ProjectManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Colabs.ProjectManagement.Persistence.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+
+        public UserRepository(ColabsDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<User> GetUserByEmailAsync(string email, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Users.FirstOrDefaultAsync(u => u.Email == email);
+            
+        }
+
+        public async Task<User> GetUserByUsernameAsync(string username, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+    }
+}

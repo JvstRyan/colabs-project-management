a2a8b43ad11eae6345f56e6e8274b296e3e7a903
Ryan Horrmann <ryanhorrmann123@gmail.com>
2025-05-02 17:47:06 +0200

    Add: User controller (register, login, getbyid)

---
+++ Colabs.ProjectManagement.Api/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Colabs.ProjectManagement.Application.Features.Auth.Commands.Login;
+using Colabs.ProjectManagement.Application.Features.Auth.Commands.Register;
+using Colabs.ProjectManagement.Application.Features.Auth.Queries.GetUserById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Colabs.ProjectManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}", Name = "GetUserById")]
+        public async Task<ActionResult<GetUserByIdResponse>> GetUserById(Guid id)
+        {
+            var getUserByIdQuery = new GetUserByIdQuery() {UserId = id};
+            var result = await _mediator.Send(getUserByIdQuery);
+            return Ok(result);
+        }
+
+        [HttpPost("register", Name = "RegisterUser")]
+        public async Task<ActionResult<RegisterUserCommandResponse>> RegisterUser(
+            [FromBody] RegisterUserCommand registerUserCommand)
+        {
+          var response = await _mediator.Send(registerUserCommand);
+          return Ok(response);
+        }
+
+        [HttpPost("login", Name = "LoginUser")]
+        public async Task<ActionResult<LoginUserCommandResponse>> LoginUser([FromBody] LoginUserCommand loginUserCommand)
+        {
+            var response = await _mediator.Send(loginUserCommand);
+            return Ok(response);
+        }
+    }
+}

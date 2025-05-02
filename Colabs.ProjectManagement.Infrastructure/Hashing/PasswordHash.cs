a485ba1bfb6e0fc33c7fe861da6f1c871426be93
Ryan Horrmann <ryanhorrmann123@gmail.com>
2025-05-02 15:41:23 +0200

    Add: Logic to hash and verify passwords

---
+++ Colabs.ProjectManagement.Infrastructure/Hashing/PasswordHash.cs
@@ -0,0 +1,19 @@
+﻿using Colabs.ProjectManagement.Application.Contracts.Utilities;
+
+namespace Colabs.ProjectManagement.Infrastructure.Hashing
+{
+    public class PasswordHash : IPasswordUtils
+    {
+        private const int WorkFactor = 12;
+        
+        public string HashPassword(string password)
+        {
+           return BCrypt.Net.BCrypt.HashPassword(password, WorkFactor);
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+           return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}

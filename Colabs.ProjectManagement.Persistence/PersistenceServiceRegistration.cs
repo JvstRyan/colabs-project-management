113178f389a54ad16fa7eec17b532f8dad01b40d
Ryan Horrmann <ryanhorrmann123@gmail.com>
2025-05-02 17:46:19 +0200

    Add: Persistence service registration

---
+++ Colabs.ProjectManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Colabs.ProjectManagement.Application.Contracts.Persistence;
+using Colabs.ProjectManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Colabs.ProjectManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ColabsDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("ColabsApplicationConnectionString")));
+            
+            services.AddScoped(typeof(IGenericRepository<>), typeof(BaseRepository<>));
+            
+            services.AddScoped<IUserRepository, UserRepository>();
+            
+            return services;
+        }
+    }
+}

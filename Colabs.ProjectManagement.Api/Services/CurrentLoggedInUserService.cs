df53f77dc560eaf5a74126d77f124ca3c0eb0ae3
Ryan Horrmann <ryanhorrmann123@gmail.com>
2025-05-04 18:43:27 +0200

    Add: HttpContextAccessor to access the userId through authorized request

---
+++ Colabs.ProjectManagement.Api/Services/CurrentLoggedInUserService.cs
@@ -0,0 +1,24 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Colabs.ProjectManagement.Application.Contracts;
+
+namespace Colabs.ProjectManagement.Api.Services
+{
+    public class CurrentLoggedInUserService : ICurrentLoggedInUserService
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public CurrentLoggedInUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _contextAccessor = httpContextAccessor;
+        }
+
+        public string UserId
+        {
+            get
+            {
+                return _contextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
+            }
+        }
+    }
+}
